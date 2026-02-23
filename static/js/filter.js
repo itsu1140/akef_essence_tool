38edd9b20661e9f9c98357e16f65e59c80841b8f
itsu1140 <n.itsuki1140@gmail.com>
2026-02-23 20:56:38 +0900

    fix: toggle action

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,10 +1,12 @@
+import { get_filtered_weapon } from "./api.js";
+
 let activeFilters = []; // ONのボタン一覧
 
 export function weapon_filter(pushed_btn) {
     if (pushed_btn.classList.contains("active")) {
         activeFilters.push(pushed_btn.dataset);
     } else {
-        activeFilters = activeFilters.filter(f => f !== btn.dataset);
+        activeFilters = activeFilters.filter(f => f !== pushed_btn.dataset);
     }
     fetch("/weapon_filter", {
         method: "POST",
